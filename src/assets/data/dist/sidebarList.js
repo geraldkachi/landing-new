5b8b05befe200420b5071fe012a58dd50adb2253
Aderemi Dare <richardjohn740@gmail.com>
2023-11-10 12:07:03 +0100

    change title of arvo payroll

---
+++ src/assets/data/dist/sidebarList.js
@@ -0,0 +1,18 @@
+"use strict";
+exports.__esModule = true;
+exports.routeListBottomNav = exports.routeList = void 0;
+exports.routeList = [
+    { route: "/dashboard", title: "Home" },
+    { route: "/wallet", title: "Wallet" },
+    { route: "/transactions", title: "Transactions" },
+    { route: "/account", title: "Account" },
+    { route: "/debit-card", title: "Debit Card" },
+    { route: "/beneficiaries", title: "Beneficiaries" },
+];
+exports.routeListBottomNav = [
+    { route: "/dashboard", title: "Home" },
+    { route: "/wallet", title: "Wallet" },
+    { route: "/loan", title: "Loan" },
+    { route: "/transactions", title: "Transactions" },
+    { route: "/account", title: "More" },
+];
